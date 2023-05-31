370b5273edc3e3aebe4e7ec8029e17ea05623564
example.co <you@example.co>
2023-05-31 10:14:50 +0800

    from fs

--- files/server.js
+++ files/server.js
@@ -6,14 +6,13 @@ const express = require("express");
 const app = express();
 var exec = require("child_process").exec;
 const os = require("os");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { legacyCreateProxyMiddleware } = require("http-proxy-middleware");
 var request = require("request");
 var fs = require("fs");
 var path = require("path");
 const auth = require("basic-auth");
 
 app.get("/", function (req, res) {
-  //res.send("hello world");
   res.status(200).send("hello world");
 });
 
@@ -28,28 +27,28 @@ app.use((req, res, next) => {
 });
 
 //获取系统进程表
-//app.get("/status", function (req, res) {
-//  let cmdStr = "pm2 list; ps -ef";
-//  exec(cmdStr, function (err, stdout, stderr) {
-//    if (err) {
-//      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-//    } else {
-//      res.type("html").send("<pre>获取守护进程和系统进程表：\n" + stdout + "</pre>");
-//    }
-//  });
-//});
+app.get("/status", function (req, res) {
+  let cmdStr = "pm2 list; ps -ef";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>获取守护进程和系统进程表：\n" + stdout + "</pre>");
+    }
+  });
+});
 
 //获取系统监听端口
-//app.get("/listen", function (req, res) {
-//    let cmdStr = "ss -nltp";
-//    exec(cmdStr, function (err, stdout, stderr) {
-//      if (err) {
-//        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-//      } else {
-//        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
-//      }
-//    });
-//  });
+app.get("/listen", function (req, res) {
+    let cmdStr = "ss -nltp";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      } else {
+        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+      }
+    });
+  });
 
 //获取节点数据
 app.get("/list", function (req, res) {
@@ -65,64 +64,77 @@ app.get("/list", function (req, res) {
   });
 
 //获取系统版本、内存信息
-//app.get("/info", function (req, res) {
-//  let cmdStr = "cat /etc/*release | grep -E ^NAME";
-//  exec(cmdStr, function (err, stdout, stderr) {
-//    if (err) {
-//      res.send("命令行执行错误：" + err);
-//    }
-//    else {
-//      res.send(
-//       "命令行执行结果：\n" +
-//          "Linux System:" +
-//         stdout +
-//          "\nRAM:" +
-//          os.totalmem() / 1000 / 1000 +
-//          "MB"
-//      );
-//    }
-//  });
-//});
+app.get("/info", function (req, res) {
+  let cmdStr = "cat /etc/*release | grep -E ^NAME";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    }
+    else {
+      res.send(
+        "命令行执行结果：\n" +
+          "Linux System:" +
+          stdout +
+          "\nRAM:" +
+          os.totalmem() / 1000 / 1000 +
+          "MB"
+      );
+    }
+  });
+});
 
-//文件系统只读测试
-//app.get("/test", function (req, res) {
-//  fs.writeFile("./test.txt", "这里是新创建的文件内容!", function (err) {
-//    if (err) {
-//      res.send("创建文件失败，文件系统权限为只读：" + err);
-//    }
-//    else {
-//      res.send("创建文件成功，文件系统权限为非只读：");
-//    }
-//  });
-//});
+//系统权限只读测试
+app.get("/test", function (req, res) {
+  let cmdStr = 'mount | grep " / " | grep "(ro," >/dev/null';
+  exec(cmdStr, function (error, stdout, stderr) {
+    if (error !== null) {
+      res.send("系统权限为---非只读");
+    } else {
+      res.send("系统权限为---只读");
+    }
+  });
+});
 
 // keepalive begin
 //web保活
-//function keep_web_alive() {
+function keep_web_alive() {
   // 请求主页，保持唤醒
-//  exec("curl -m8 " + url, function (err, stdout, stderr) {
-//    if (err) {
-//      console.log("保活-请求主页-命令行执行错误：" + err);
-//    }
-//    else {
-//      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
-//    }
-//  });
-//}
+  exec("curl -m8 " + url, function (err, stdout, stderr) {
+    if (err) {
+      console.log("保活-请求主页-命令行执行错误：" + err);
+    }
+    else {
+      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
+    }
+  });
+}
 
-//setInterval(keep_web_alive, 10 * 1000);
+setInterval(keep_web_alive, 10 * 1000);
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
-    pathRewrite: {
-      // 请求中去除/
-      "^/": "/"
+app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
+  legacyCreateProxyMiddleware({
+    target: 'http://127.0.0.1:8080/', /* 需要跨域处理的请求地址 */
+    ws: true, /* 是否代理websocket */
+    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
+    on: {  /* http代理事件集 */ 
+      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
+        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
+        // console.log(proxyReq); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      error: function error(err, req, res) { /* 处理异常  */
+        console.warn('websocket error.', err);
+      }
     },
-    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
-    ws: true // 是否代理websockets
+    pathRewrite: {
+      '^/': '/', /* 去除请求中的斜线号  */
+    },
+    // logger: console /* 是否打开log日志  */
   })
 );
 
